b70773854ca0154bce1c3f41de936618cd762b8b
craigongit <nbenicraig@gmail.com>
2026-03-16 19:32:47 +0200

    Commit v1

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'trading-calc-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdn.tailwindcss.com',
+  'https://esm.sh/react@18',
+  'https://esm.sh/react-dom@18/client'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
